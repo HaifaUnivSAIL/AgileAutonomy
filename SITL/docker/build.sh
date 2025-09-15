14dd2f3da3f955184ebe5325e93697a47b1bfeae
JonnyScream <jo.muller11@gmail.com>
2025-09-15 18:31:00 +0300

    SITL (Software In The Loop) folder added.
    Adding the docker files for the SITL installation.
    according to the following links:
    1. https://ardupilot.org/dev/docs/setting-up-sitl-on-linux.html
    2. https://ardupilot.org/dev/docs/building-setup-linux.html#ccache-for-faster-builds
    
    In order to create the SITL docker run the followings:
    1. ./build.sh
    2. ./launch.sh

---
+++ SITL/docker/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Clone ArduPilot locally if not already present
+if [ ! -d "./ardupilot" ]; then
+    git clone --recurse-submodules https://github.com/ArduPilot/ardupilot.git
+fi
+
+# Build Docker image
+docker build . -t ardupilot-builder --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g)
+sudo chmod +x ./launch.sh
---
+++ SITL/docker/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+docker run --rm -it \
+  -v $(pwd)/ardupilot:/home/builder/ardupilot \
+  -u $(id -u):$(id -g) \
+  ardupilot-builder \
+  bash
+
+cd ~/ardupilot
+
+./waf configure --board MatekH743
+./waf copter
+
+The first configure command should be called only once or when you want to change a
+configuration option. One configuration often used is the `--board` option to
+switch from one board to another one. For example we could switch to
+SkyViper GPS drone and build again:
+
+./waf configure --board skyviper-v2450
+./waf copter
+
