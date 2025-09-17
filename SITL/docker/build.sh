5d702c0e4f67f888725d3299bf0d6c4e820e95d3
JonnyScream <jo.muller11@gmail.com>
2025-09-17 18:22:00 +0300

    Necessary installations for displaying SITL simulator GUI.
    1. Latest Anaconda version.
    2. docker launch with the x11 and display.
    3. Many more.

--- SITL/docker/build.sh
+++ SITL/docker/build.sh
@@ -6,6 +6,8 @@ if [ ! -d "./ardupilot" ]; then
     git clone --recurse-submodules https://github.com/ArduPilot/ardupilot.git
 fi
 
+xhost +local:docker
+
 # Build Docker image
 docker build . -t ardupilot-builder --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g)
 sudo chmod +x ./launch.sh
--- SITL/docker/launch.sh
+++ SITL/docker/launch.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 docker run --rm -it \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $(pwd)/ardupilot:/home/builder/ardupilot \
   -w /home/builder/ardupilot \
   -u $(id -u):$(id -g) \
   ardupilot-builder \
   bash
-
-cd ~/ardupilot
-
-./waf configure --board MatekH743
-./waf copter
-
-The first configure command should be called only once or when you want to change a
-configuration option. One configuration often used is the `--board` option to
-switch from one board to another one. For example we could switch to
-SkyViper GPS drone and build again:
-
-./waf configure --board skyviper-v2450
-./waf copter
 
